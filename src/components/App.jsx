2f4ed0445c64e872a2f2e45da850d00dac737ee9
Roman-Nechaev <nrchaevroman@gmail.com>
2023-03-14 22:11:02 +0200

    add TodoList

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,11 @@
+import React, { Component } from 'react';
+
 import Counter from './Counter/Counter';
 import Dropdown from './Dropdown/Dropdown';
 import ColorPicker from './ColorPicker/ColorPicker';
+import TodoList from './TodoList/TodoList';
+
+import initialTodos from '../todos.json';
 
 const colorPickerOptions = [
   { label: 'red', color: '#F44336' },
@@ -11,13 +16,41 @@ const colorPickerOptions = [
   { label: 'indigo', color: '#3F51B5' },
 ];
 
-export const App = () => {
-  return (
-    <>
-      <h1>Состояние компонента</h1>
-      {/* <Counter initialValue={10} /> */}
-      {/* <Dropdown /> */}
-      <ColorPicker options={colorPickerOptions} />
-    </>
-  );
-};
+class App extends Component {
+  state = {
+    todos: initialTodos,
+  };
+
+  deleteTodo = todoId => {
+    this.setState(prevState => ({
+      todos: prevState.todos.filter(todo => todo.id !== todoId),
+    }));
+  };
+
+  render() {
+    const { todos } = this.state;
+
+    const totalTodoCount = todos.length;
+    const completedTodo = todos.reduce(
+      (acc, todo) => (todo.completed ? acc + 1 : acc),
+      0
+    );
+    console.log(completedTodo);
+    return (
+      <>
+        <h1>Состояние компонента</h1>
+        {/* <Counter initialValue={10} /> */}
+        {/* <Dropdown /> */}
+        {/* <ColorPicker options={colorPickerOptions} /> */}
+        <div>
+          <p>Общее кол-во: {totalTodoCount}</p>
+          <p>Кол-во выполненных: {completedTodo} </p>
+        </div>
+
+        <TodoList todos={todos} onDeleteTodo={this.deleteTodo} />
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/TodoList/TodoList.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './TodoList.css';
+
+const TodoList = ({ todos, onDeleteTodo }) => (
+  <ul className="TodoList">
+    {todos.map(({ id, text }) => (
+      <li key={id} className="TodoList__item">
+        <p className="TodoList__text">{text}</p>
+        <button
+          type="button"
+          onClick={() => {
+            onDeleteTodo(id);
+          }}
+        >
+          Удалить
+        </button>
+      </li>
+    ))}
+  </ul>
+);
+
+export default TodoList;
